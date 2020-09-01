8f6c26ee7ddb9a8fdde844572c27aeb86d5bc4b8
Ana-Morales <moralesanaruth@gmail.com>
2020-09-01 22:53:59 +0000

    removing the character at a given position

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -2,5 +2,5 @@
 def remove_char_at(str, n):
     str_copy = str
     if n >= 0 and n < len(str):
-        str_copy = str_copy.replace(str_copy[n], "")
+        str_copy = str_copy[:n] + str_copy[(n+1):]
     return str_copy
