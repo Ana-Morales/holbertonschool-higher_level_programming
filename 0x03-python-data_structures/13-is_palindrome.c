3b80fee5074d227030542b705fa5c1d21f76d013
Ana-Morales <moralesanaruth@gmail.com>
2020-09-08 23:18:51 +0000

    Fix for efficiency

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,35 +3,60 @@
 #include "lists.h"
 
 /**
+ * rev_listint -  reverses a listint_t linked list
+ * @mid: pointer to first node
+ *
+ * Return: a pointer to the first node of the reversed list
+ */
+listint_t *rev_listint(listint_t **mid)
+{
+	listint_t *prev, *next;
+
+	if ((*mid)->next == NULL)
+		return (*mid);
+	prev = *mid;
+	next = (*mid)->next;
+	prev->next = NULL;
+	while (next != NULL)
+	{
+		*mid = next;
+		next = next->next;
+		(*mid)->next = prev;
+		prev = *mid;
+	}
+	return (*mid);
+}
+/**
  * is_palindrome - checks if a singly linked list is a palindrome
  * @head: pointer to head of list
  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *end = *head;
-	int len = 0, i = 0, j;
+	listint_t *end = *head, *ini = *head, *mid = *head;
+	int len = 0, i = 0;
 
-	if (*head == NULL)
+	if (*head == NULL || (*head)->next == NULL)
 		return (1);
-	while (end->next != NULL)
+	while (end != NULL)
 	{
 		end = end->next;
 		len++;
 	}
-	while (i <= len / 2)
+	if (len % 2 == 0)
+		len++;
+	while (i < len / 2)
 	{
-		if ((*head)->n != end->n)
-			return (0);
-		*head = (*head)->next;
-		end = *head;
-		j = 0;
-		while (j < (len - (2 * i) - 2))
-		{
-			end = end->next;
-			j++;
-		}
+		mid = mid->next;
 		i++;
+	}
+	rev_listint(&mid);
+	while (ini != NULL && mid != NULL)
+	{
+		if (ini->n != mid->n)
+			return (0);
+		ini = ini->next;
+		mid = mid->next;
 	}
 	return (1);
 }
