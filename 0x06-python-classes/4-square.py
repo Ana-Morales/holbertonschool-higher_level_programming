d4ee292d154872edbe913096b2964d5f0199944b
Ana-Morales <moralesanaruth@gmail.com>
2020-09-22 21:31:20 +0000

    Class Square, area method, get and set

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""This module writes a class Square that defines a square"""
+
+
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size=0):
+        """Initializes the data
+        Args:
+            size: size of the square
+        """
+
+        self.size = size
+
+    @property
+    def size(self):
+        """Getting size value"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """Setting size value"""
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Returns the area of a square"""
+
+        return self.__size * self.__size
