79775cca2848a0e05d761b728388b6224cace3cb
Ana-Morales <moralesanaruth@gmail.com>
2020-09-22 21:13:59 +0000

    Class to define a square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module writes a class Square that defines a square"""
+
+
+class Square:
+    """Defines a square by:
+    Private instance attribute: size"""
+
+    def __init__(self, size):
+        """Initializes the data."""
+
+        self.__size = size
