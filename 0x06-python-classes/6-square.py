8ebaf3f2a6f88fc7260631e10a6be23e12a11675
Ana-Morales <moralesanaruth@gmail.com>
2020-09-22 22:20:19 +0000

    Fix to check if elements of the tuple are integers

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -40,6 +40,8 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         elif len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value[0]) != int or type(value[1]) != int:
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
