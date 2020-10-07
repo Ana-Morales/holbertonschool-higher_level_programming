62521eb9e8475835772ae67894407be9cb745aa3
Ana-Morales <moralesanaruth@gmail.com>
2020-10-07 00:16:27 +0000

    Fix pep8

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,6 +4,7 @@ is an instance of a class that inherited (directly or indirectly)
 from the specified class ; otherwise False
 """
 
+
 def inherits_from(obj, a_class):
     """Defines inherits_from function"""
     return issubclass(type(obj), a_class) and (type(obj) != a_class)
