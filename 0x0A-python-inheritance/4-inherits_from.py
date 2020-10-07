9856d5e274f46edec4924523caa1e6c5e9e03de2
Ana-Morales <moralesanaruth@gmail.com>
2020-10-07 00:15:52 +0000

    Fix pep8

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 """This module defines a function that returns True if the object
 is an instance of a class that inherited (directly or indirectly)
-from the specified class ; otherwise False"""
-
+from the specified class ; otherwise False
+"""
 
 def inherits_from(obj, a_class):
-    """returns True if the object is an instance of a class that inherited
-    (directly or indirectly) from the specified class ; otherwise False"""
-
+    """Defines inherits_from function"""
     return issubclass(type(obj), a_class) and (type(obj) != a_class)
