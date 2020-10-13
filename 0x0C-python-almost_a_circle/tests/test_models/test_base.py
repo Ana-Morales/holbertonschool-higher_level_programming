9b54643620fa6a1b2a0f600a3a3cc9b23cac12ee
Ana-Morales <moralesanaruth@gmail.com>
2020-10-13 21:15:29 +0000

    Add tests for squares

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -5,6 +5,7 @@ import json
 import contextlib
 from models.base import Base
 from models.rectangle import Rectangle
+from models.square import Square
 from io import StringIO
 
 
@@ -55,10 +56,15 @@ class TestBaseCls(unittest.TestCase):
         r1 = Rectangle(10, 7, 2, 8)
         r2 = Rectangle(2, 4)
         Rectangle.save_to_file([r1, r2])
-
         with open("Rectangle.json", "r") as f:
             self.assertEqual(json.dumps([r1.to_dictionary(),
                                          r2.to_dictionary()]), f.read())
+        s1 = Square(10, 2, 8)
+        s2 = Square(2)
+        Square.save_to_file([s1, s2])
+        with open("Square.json", "r") as f:
+            self.assertEqual(json.dumps([s1.to_dictionary(),
+                                         s2.to_dictionary()]), f.read())
 
     def test_create(self):
         """Testing create method"""
@@ -68,6 +74,14 @@ class TestBaseCls(unittest.TestCase):
         with StringIO() as buf, contextlib.redirect_stdout(buf):
             r2 = Rectangle.create(**r1_dictionary)
             print(r2)
+            self.assertEqual(buf.getvalue(), expected)
+
+        s1 = Square(3, 1, 0, 1)
+        s1_dictionary = s1.to_dictionary()
+        expected = "[Square] (1) 1/0 - 3\n"
+        with StringIO() as buf, contextlib.redirect_stdout(buf):
+            s2 = Square.create(**s1_dictionary)
+            print(s2)
             self.assertEqual(buf.getvalue(), expected)
 
     def test_load_from_file(self):
@@ -82,6 +96,17 @@ class TestBaseCls(unittest.TestCase):
         self.assertEqual(len(list_rectangles_output), 2)
         self.assertIsInstance(list_rectangles_output[0], Rectangle)
         self.assertEqual(list_rectangles_output[0].width, 10)
+
+        Base._Base__nb_objects = 0
+        s1 = Square(10, 2, 8)
+        s2 = Square(2)
+        list_squares_input = [s1, s2]
+        Square.save_to_file(list_squares_input)
+        list_squares_output = Square.load_from_file()
+        self.assertTrue(type(list_squares_output) is list)
+        self.assertEqual(len(list_squares_output), 2)
+        self.assertIsInstance(list_squares_output[0], Square)
+        self.assertEqual(list_squares_output[0].size, 10)
 
 if __name__ == '__main__':
     unittest.main()
