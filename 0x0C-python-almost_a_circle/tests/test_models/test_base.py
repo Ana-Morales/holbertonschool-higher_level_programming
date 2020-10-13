3e1ccb7e089e85d75949d1c51013bce8e9e277ae
Ana-Morales <moralesanaruth@gmail.com>
2020-10-13 21:02:42 +0000

    Add test for methods: create, save_to_file, load_from_file

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """Unittest for Base"""
 import unittest
+import json
+import contextlib
 from models.base import Base
+from models.rectangle import Rectangle
+from io import StringIO
 
 
 class TestBaseCls(unittest.TestCase):
@@ -45,6 +49,39 @@ class TestBaseCls(unittest.TestCase):
         json_string = '[{"height": 4, "width": 10, "id": 89}]'
         ls = Base.from_json_string(json_string)
         self.assertTrue(type(ls) is list)
+
+    def test_save_to_file(self):
+        """Testing save to file method"""
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        Rectangle.save_to_file([r1, r2])
+
+        with open("Rectangle.json", "r") as f:
+            self.assertEqual(json.dumps([r1.to_dictionary(),
+                                         r2.to_dictionary()]), f.read())
+
+    def test_create(self):
+        """Testing create method"""
+        r1 = Rectangle(3, 5, 1, 0, 1)
+        r1_dictionary = r1.to_dictionary()
+        expected = "[Rectangle] (1) 1/0 - 3/5\n"
+        with StringIO() as buf, contextlib.redirect_stdout(buf):
+            r2 = Rectangle.create(**r1_dictionary)
+            print(r2)
+            self.assertEqual(buf.getvalue(), expected)
+
+    def test_load_from_file(self):
+        """Testing load from file method"""
+        Base._Base__nb_objects = 0
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        list_rectangles_input = [r1, r2]
+        Rectangle.save_to_file(list_rectangles_input)
+        list_rectangles_output = Rectangle.load_from_file()
+        self.assertTrue(type(list_rectangles_output) is list)
+        self.assertEqual(len(list_rectangles_output), 2)
+        self.assertIsInstance(list_rectangles_output[0], Rectangle)
+        self.assertEqual(list_rectangles_output[0].width, 10)
 
 if __name__ == '__main__':
     unittest.main()
