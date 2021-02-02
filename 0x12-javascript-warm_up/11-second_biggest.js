6b03db3ee5441b7c1d20059e277c4aef1ac2c4d7
Ana-Morales <moralesanaruth@gmail.com>
2021-02-02 19:10:34 +0000

    Fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@ if (process.argv.length <= 3) {
   console.log(0);
 } else {
   const args = process.argv.slice(2);
-  args.sort();
+  args.sort(function(a, b){return a-b});
   args.reverse();
   console.log(args[1]);
 }
